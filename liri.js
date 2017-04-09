a8518c0fb951e4e6bffeecc6e9c14a6d049ab692
Jing Huang <jinghuang@Jings-MacBook-Pro.local>
2017-04-09 17:10:49 -0400

    finished

--- liri.js
+++ liri.js
@@ -2,8 +2,48 @@ var app = (function(){
 
     //importing keys
     var client = require("./keys.js"),
-        song = require('spotify');
-        movie = require("request");
+        song = require('spotify'),
+        rp = require("request-promise"),
+        fs = require("fs");
+
+    var readFile = fs.readFileSync("random.txt", "utf8");
+
+    fs.appendFile("log.txt", "\n" + "Command: " + process.argv[2] + "\n" + "Search: " + process.argv[3] + "\n" + "--------------------", function(err){
+           if (err) return console.log(err);
+    });
+
+
+    //LiriBot
+    var liriBot = function(){
+        console.log(readFile);
+
+        var songTitle = readFile.split(",");
+        console.log(songTitle[1]);
+
+        var params = {
+            type: "track",
+            query: songTitle[1]
+        }
+
+        if(!params.query){
+            params.query = "The Sign"
+        }
+        song.search(params, function(error, data){
+            // console.log(data);
+
+            for (var i = 0; i < data.tracks.items.length; i++){
+
+                var need = data.tracks.items[i];
+                console.log("Artist: " + need.album.artists[0].name);
+                console.log("Album: " + need.album.name);
+                console.log("Song's name: " + need.name);
+                console.log("Preview song: " + need.preview_url);
+                console.log("------------------------------------------------------");
+                console.log("------------------------------------------------------");
+
+            }
+        });
+    }
 
 
     //grabbing tweets
@@ -57,7 +97,8 @@ var app = (function(){
     }
 
     //searching songs
-    var spotify = function () {
+    var spotify = function (songsong) {
+        var defualtSong =function(){
         var params = {
             type: "track",
             query: process.argv[3]
@@ -81,21 +122,52 @@ var app = (function(){
 
             }
         });
+        }
     }
 
 
     //lookup movies
     var movie = function(){
-        console.log("Hello!")
+
+       var search =  process.argv[3];
+
+       if (!search){
+           search = "Mr. Nobody";
+       }
+       var promise = rp ("http://www.omdbapi.com/?t=" + search);
+       promise.then(function (data){
+           var data = JSON.parse(data);
+
+           try{
+               if(!data.Title){
+                   throw console.log("Movie title not found!");
+               }
+           }
+           catch(error){
+            console.log(err.message);
+           }
+
+          console.log("Movie title: " + data.Title);
+          console.log("Movie released: " + data.Year);
+          console.log("IMDB rating: " + data.imdbRating);
+                    data.Ratings.map(function (el, index, arr){
+              if(el.Source == "Rotten Tomatoes"){
+                  console.log("Rotten Tomatoes Rating: " + el.Value);
+              } else if (!el.Source){
+                  console.log("Rotten Tomatoes Rating: N/A");
+              }
+          });
+          console.log("Movie plot: " + data.Plot);
+          console.log("Movie Country: " + data.Country);
+          console.log("Movie language: " + data.Language);
+          console.log("Actors: "+ data.Actors);
+       })
+
+       .catch(function(err){
+           console.log(err.message);
+       })
 
     }
-
-
-
-
-
-
-
 
 
 
@@ -108,7 +180,8 @@ var app = (function(){
         spotify: spotify,
         post: post, 
         search: search,
-        movie: movie
+        movie: movie,
+        liriBot: liriBot
     }
 
 
@@ -136,6 +209,10 @@ switch(process.argv[2]){
 
     case "movie-this":
     app.movie();
+    break;
+
+    case "do-what-it-says":
+    app.liriBot();
     break;
 
 
